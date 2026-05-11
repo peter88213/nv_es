c173ad668863d6eeb6902cf926336c609f83f6f8
peter88213 <petert88213@gmail.com>
2026-05-11 10:56:04 +0200

    Updated the settings.

--- tools/settings.py
+++ tools/settings.py
@@ -1,15 +1,15 @@
 """Language pack settings"""
 
-languageName = 'Vulcanian'
+languageName = 'Spanish'
 # the name of the language
 
 languageCode = 'es'
 # the language code according to ISO 639-1
 
-provider = "T'Pol t_pol@enterprise.starfleet"
+provider = "Unknown"
 # the name of the translation's provider
 
-lastTranslator = "T'Pol t_pol@enterprise.starfleet"
+lastTranslator = "Unknown"
 # the name of the last translator
 
 __all__ = ['languageName', 'languageCode', 'provider', 'lastTranslator']
